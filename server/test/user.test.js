65fb00d636d8572a9751d248d4ae4f1d33deb9a7
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-05-31 20:55:38 -0300

    test: user register with success

---
+++ server/test/user.test.js
@@ -0,0 +1,38 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+const express = require("express");
+const app = express();
+const router = require("../src/app");
+const supertest = require("supertest");
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(router);
+
+const request = supertest(app.listen(3002,()=>{
+	console.log("User tests...");
+}));
+
+
+describe("User registers suites",()=>{
+
+	test("Should register a new user with success .",()=>{
+		let time = Date.now();
+		let email = `${time}@gmail.com`;
+		let user = {
+			name:"benjamim",
+			email:email,
+			password:`${Date.now()}`};
+		return request.post("/user")
+			.send(user)
+			.then(res=>{
+				expect(res.statusCode).toEqual(201);
+				expect(res.body.email).toEqual(email);
+			})
+			.catch(error=>{
+				fail(error);
+			});
+	});
+	
+});
+
