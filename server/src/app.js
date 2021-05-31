f04f980062a2743b8c272f443017d06d1cafd117
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-05-31 20:40:27 -0300

    feat: routes

---
+++ server/src/app.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+const express = require("express");
+const router = express.Router();
+
+router.use((req, res, next)=> {
+	next();
+});
+
+module.exports = router;
