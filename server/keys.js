f15968b9e0803a97255be20dd5835c6452945dc4
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-05-31 20:49:38 -0300

    env: env vars

---
+++ server/keys.js
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+module.exports = {
+	apiPort:process.env.API_PORT,
+	secret:process.env.SECRET,
+	mgHost: process.env.HOST,
+	mgDatabase: process.env.MONGO_INITDB_DATABASE,
+	mgPort: process.env.ME_CONFIG_MONGODB_PORT
+};
