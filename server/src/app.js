e530f2c85707ca39d9aecee5faa75b5d04bf1d30
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-06-01 09:41:50 -0300

    feat(app): create and delete tool

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,14 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("./controllers/UserController");
+const ToolController = require("./controllers/ToolController")
 
 router.use((req, res, next)=> {
 	next();
 });
 
+router.delete("/tool",ToolController.delete);
+router.post("/tool",ToolController.create);
 router.post("/user",UserController.create);
 router.post("/auth",UserController.auth);
 router.delete("/user:email",UserController.delete);
