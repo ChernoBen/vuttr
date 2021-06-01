5da7b15cdb27f65178edc31a898197e615fd173c
ChernoBen <bneto@computacao.cua.ufmt.br>
2021-05-31 21:04:45 -0300

    feat(app): UserControler.create

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,12 @@
 /* eslint-disable linebreak-style */
 const express = require("express");
 const router = express.Router();
+const UserController = require("./controllers/UserController");
 
 router.use((req, res, next)=> {
 	next();
 });
+
+router.post("/user",UserController.create);
 
 module.exports = router;
